7a865ca82e26e8aef95c652417a837ec4877d488
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2024-11-05 23:30:59 +0800

    stash-4-0test

--- yami-shop-security/yami-shop-security-api/src/main/java/com/yami/shop/security/api/controller/LoginController.java
+++ yami-shop-security/yami-shop-security-api/src/main/java/com/yami/shop/security/api/controller/LoginController.java
@@ -3,24 +3,22 @@ package com.yami.shop.security.api.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.yami.shop.bean.model.User;
 import com.yami.shop.common.exception.YamiShopBindException;
+import com.yami.shop.common.response.ServerResponseEntity;
 import com.yami.shop.common.util.PrincipalUtil;
 import com.yami.shop.dao.UserMapper;
 import com.yami.shop.security.common.bo.UserInfoInTokenBO;
 import com.yami.shop.security.common.dto.AuthenticationDTO;
 import com.yami.shop.security.common.enums.SysTypeEnum;
 import com.yami.shop.security.common.manager.PasswordCheckManager;
-import com.yami.shop.security.common.manager.PasswordManager;
 import com.yami.shop.security.common.manager.TokenStore;
 import com.yami.shop.security.common.vo.TokenInfoVO;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.yami.shop.common.response.ServerResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.validation.Valid;
 
 /**
  * @author 菠萝凤梨
@@ -38,20 +36,18 @@ public class LoginController {
     @Autowired
     private PasswordCheckManager passwordCheckManager;
 
-    @Autowired
-    private PasswordManager passwordManager;
 
     @PostMapping("/login")
-    @Operation(summary = "账号密码(用于前端登录)" , description = "通过账号/手机号/用户名密码登录，还要携带用户的类型，也就是用户所在的系统")
+    @Operation(summary = "账号密码(用于前端登录)", description = "通过账号/手机号/用户名密码登录，还要携带用户的类型，也就是用户所在的系统")
     public ServerResponseEntity<TokenInfoVO> login(
             @Valid @RequestBody AuthenticationDTO authenticationDTO) {
         String mobileOrUserName = authenticationDTO.getUserName();
         User user = getUser(mobileOrUserName);
 
-        String decryptPassword = passwordManager.decryptPassword(authenticationDTO.getPassWord());
+        String decryptPassword = authenticationDTO.getPassWord();
 
         // 半小时内密码输入错误十次，已限制登录30分钟
-        passwordCheckManager.checkPassword(SysTypeEnum.ORDINARY,authenticationDTO.getUserName(), decryptPassword, user.getLoginPassword());
+        passwordCheckManager.checkPassword(SysTypeEnum.ORDINARY, authenticationDTO.getUserName(), decryptPassword, user.getLoginPassword());
 
         UserInfoInTokenBO userInfoInToken = new UserInfoInTokenBO();
         userInfoInToken.setUserId(user.getUserId());
@@ -69,7 +65,7 @@ public class LoginController {
             user = userMapper.selectOne(new LambdaQueryWrapper<User>().eq(User::getUserMobile, mobileOrUserName));
         }
         // 如果不是手机验证码登陆， 找不到手机号就找用户名
-        if  (user == null) {
+        if (user == null) {
             user = userMapper.selectOneByUserName(mobileOrUserName);
         }
         if (user == null) {
